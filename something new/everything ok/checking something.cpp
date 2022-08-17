9b52d5f67c3a37d80ee7d17b5ae385eafb17e956
jahirnstu14 <jahirkhan.ice.nstu.student14@gmail.com>
2022-08-17 11:50:17 +0600

    This is a new commit for what I originally planned to be an amendmend

---
+++ something new/everything ok/checking something.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+int a,b,c;
+cin>>a>>b;
+cout<<"my name is jahirul islam"<<endl;
+
+cout<<"addition of two number:"<<a+hgggggggggggggggggggggggggggb<<endl;
+cout<<@jak tomi valo thaiko"<<endl;
+
+
+
+
+
+
+}
